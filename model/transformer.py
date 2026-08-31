67e3096986bfcacb3e772f6c2b63a3445699f6ef
Shane Lowry <shane.lowry@orangesands.com>
2026-08-31 13:27:09 -0700

    Combine token and positinal embeddings

--- model/transformer.py
+++ model/transformer.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 
 class TokenEmbedding(nn.Module):
@@ -7,3 +8,32 @@ class TokenEmbedding(nn.Module):
 
     def forward(self, tokens):
         return self.embedding(tokens)
+
+class PositionalEmbedding(nn.Module):
+    def __init__(self, context_length: int, embedding_dim: int):
+        super().__init__()
+        self.embedding = nn.Embedding(context_length, embedding_dim)
+
+    def forward(self, positions):
+        return self.embedding(positions) 
+
+class InputEmbedding(nn.Module):
+    def __init__(
+            self,
+            vocab_size: int,
+            context_length: int,
+            embedding_dim: int,
+    ):
+
+        super().__init__()
+
+        self.token_embedding = TokenEmbedding(vocab_size, embedding_dim)
+        self.position_embedding = PositionalEmbedding(context_length, embedding_dim)
+
+    def forward(self, tokens):
+        positions = torch.arange(tokens.shape[1], device=tokens.device)
+
+        token_vectors = self.token_embedding(tokens)
+        position_vectors = self.position_embedding(positions)
+
+        return token_vectors + position_vectors
--- tests/test_transformer.py
+++ tests/test_transformer.py
@@ -1,9 +1,31 @@
 import torch
 
-from model.transformer import TokenEmbedding
+from model.transformer import TokenEmbedding, PositionalEmbedding, InputEmbedding
 
 def test_embedding_output_shape():
     embedding = TokenEmbedding(vocab_size=256, embedding_dim=128)
+
+    tokens = torch.tensor([[1, 2, 3, 4]])
+
+    output = embedding(tokens)
+
+    assert output.shape == (1, 4, 128)
+
+def test_positional_embedding_output_shape():
+    embedding = PositionalEmbedding(context_length=128, embedding_dim=128)
+
+    positions = torch.arange(4)
+
+    output = embedding(positions)
+
+    assert output.shape == (4, 128)
+
+def test_input_embedding_output_shape():
+    embedding = InputEmbedding(
+        vocab_size=256,
+        context_length=128,
+        embedding_dim=128,
+    )
 
     tokens = torch.tensor([[1, 2, 3, 4]])
 
